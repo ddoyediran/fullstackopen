ac784157e0f538054e06718f56eba69e9c50044f
ddoyediran <ddoyediran@gmail.com>
2022-12-12 11:43:54 +0100

    updated

--- part-3/index.js
+++ part-3/index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -21,13 +22,17 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // https://fullstackopen.com/en/part3/node_js_and_express#express
