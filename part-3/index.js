2f7c559179f7ca911ba3ae140a5c2df9fe11d263
ddoyediran <ddoyediran@gmail.com>
2022-12-15 14:49:49 +0100

    more commit

--- part-3/index.js
+++ part-3/index.js
@@ -60,9 +60,24 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  const note = request.body;
-  note.id = maxId + 1;
+  //   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  //   const note = request.body;
+  //   note.id = maxId + 1;
+
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 
@@ -76,3 +91,14 @@ app.listen(PORT, () => {
 });
 
 // https://fullstackopen.com/en/part3/node_js_and_express#nodemon
+
+/**
+ *
+ * @param {no params}
+ * @returns {integer}
+ */
+function generateId() {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
+  return maxId + 1;
+}
