a4355d2242fda506f4f503a38bc424fdc6273538
ddoyediran <ddoyediran@gmail.com>
2022-12-20 00:21:03 +0100

    more commit

--- part-3/index.js
+++ part-3/index.js
@@ -1,8 +1,10 @@
 const { response, request } = require("express");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
@@ -92,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
