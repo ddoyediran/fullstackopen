35d63cba0ac01dcde23e096cc2f5b5734f63212c
ddoyediran <ddoyediran@gmail.com>
2023-01-06 23:19:41 +0100

    index.js updated

--- part-3/index.js
+++ part-3/index.js
@@ -45,29 +45,29 @@ const requestLogger = (request, response, next) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(requestLogger);
+// app.use(requestLogger);
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-05-30T17:30:31.098Z",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2022-05-30T18:39:34.091Z",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-05-30T19:20:14.298Z",
+//     important: true,
+//   },
+// ];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -98,7 +98,7 @@ app.get("/api/notes", (request, response) => {
 // GET: fetches a single resource
 app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then((note) => {
-    response.json(note);
+    return response.json(note);
   });
 });
 
@@ -109,7 +109,7 @@ app.delete("/api/notes/:id", (request, response) => {
     return note.id != id;
   });
 
-  response.status(204).end();
+  return response.status(204).end();
 });
 
 app.post("/api/notes", (request, response) => {
@@ -126,7 +126,7 @@ app.post("/api/notes", (request, response) => {
   });
 
   note.save().then((savedNote) => {
-    response.json(savedNote);
+    return response.json(savedNote);
   });
 });
 
