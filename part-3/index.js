cd0a93e4bf51c2b82e0aa67c86db527e53d060eb
ddoyediran <ddoyediran@gmail.com>
2023-02-03 18:25:14 +0100

    clean up

--- part-3/index.js
+++ part-3/index.js
@@ -1,19 +1,11 @@
-// const { response, request } = require("express");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const Note = require("./models/note");
-// const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
-// const password = process.argv[2];
-
-// const url = `mongodb+srv://noteapp:${password}@cluster0.4idts.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-// const Note = mongoose.model("Note", noteSchema);
 
 // Middleware declaration
 const requestLogger = (request, response, next) => {
@@ -51,27 +43,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(requestLogger);
 app.use(express.static("build"));
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-05-30T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-05-30T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-05-30T19:20:14.298Z",
-//     important: true,
-//   },
-// ];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -83,20 +54,6 @@ app.get("/api/notes", (request, response) => {
   });
   return;
 });
-
-// // GET: fetches a single resource
-// app.get("/api/notes/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   console.log(id);
-//   const note = notes.find((note) => {
-//     return note.id === id;
-//   });
-//   if (note) {
-//     return response.json(note);
-//   }
-
-//   return response.status(404).end();
-// });
 
 // GET: fetches a single resource
 app.get("/api/notes/:id", (request, response, next) => {
@@ -119,7 +76,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-// POST
+// POST Add a new resource
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
