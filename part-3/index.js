1b633aed144639b93965ab5cd932bb2017b1f31d
ddoyediran <ddoyediran@gmail.com>
2023-02-03 17:34:56 +0100

    add error handler

--- part-3/index.js
+++ part-3/index.js
@@ -151,4 +151,8 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformatted id" });
   }
+
+  next(error);
 };
+
+app.use(errorHandler);
