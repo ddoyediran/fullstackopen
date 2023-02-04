20e04fbf3e15f363d63d9a79ceacc63f41646ad6
ddoyediran <ddoyediran@gmail.com>
2023-02-04 18:30:40 +0100

    validation

--- part-3/index.js
+++ part-3/index.js
@@ -31,6 +31,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -77,12 +79,12 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 // POST Add a new resource
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
+  // if (body.content === undefined) {
+  //   return response.status(400).json({ error: "content missing" });
+  // }
 
   const note = new Note({
     content: body.content,
@@ -90,21 +92,28 @@ app.post("/api/notes", (request, response) => {
     date: new Date(),
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // PUT - Update the database
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
+  // const note = {
+  //   content: body.content,
+  //   important: body.important,
+  // };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
