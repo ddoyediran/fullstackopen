45b7d7e00c5a9d553e85477f7af298c86a2d3463
ddoyediran <ddoyediran@gmail.com>
2023-02-06 13:57:57 +0100

    structure folder

--- part-3/index.js
+++ part-3/index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const Note = require("./models/note");
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
 
 const app = express();
 
