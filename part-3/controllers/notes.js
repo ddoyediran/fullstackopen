9e47d8ca033bf4307c70655303f16884ad085c59
ddoyediran <ddoyediran@gmail.com>
2023-02-20 22:15:03 +0100

    add async-await

--- part-3/controllers/notes.js
+++ part-3/controllers/notes.js
@@ -1,11 +1,19 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
-notesRouter.get("/", (request, response) => {
+/**
+ * 
+ * notesRouter.get("/", (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
   return;
+});
+ */
+
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
 // GET: fetches a single resource
