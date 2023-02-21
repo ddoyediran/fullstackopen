5963c30b493604fe473dcf2a1eb74332ff4f87bf
ddoyediran <ddoyediran@gmail.com>
2023-02-21 20:27:30 +0100

    fixed beforeEach

--- part-3/test/note_api.test.js
+++ part-3/test/note_api.test.js
@@ -9,10 +9,16 @@ const Note = require("../models/note");
 // delete all data in the database and add new ones
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+
+  const noteObjects = helper.initialNotes.map((note) => new Note(note));
+  const promiseArray = noteObjects.map((note) => note.save());
+  await Promise.all(promiseArray);
+
+  //   helper.initialNotes.forEach(async (note) => {
+  //     let noteObject = new Note(note);
+  //     await noteObject.save();
+  //     console.log("saved");
+  //   });
 });
 
 // test("notes are returned as json", async () => {
